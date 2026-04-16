eba7d3fefc1812583106d25808ec221b36adec71
Blackshadow <blackshadow0916@yandex.ru>
2026-04-16 23:33:00 +0300

    Directions DONE

--- Academy/Models/Direction.cs
+++ Academy/Models/Direction.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Academy.Models
 {
     public class Direction
     {
         [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public byte direction_id { get; set; }
-        public string direction_name { get; set; }
+        public string? direction_name { get; set; }
     }
 }
