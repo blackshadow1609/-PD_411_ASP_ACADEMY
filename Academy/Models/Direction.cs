337da136602305d48a273aaec9e9da066c3d393e
Blackshadow <blackshadow0916@yandex.ru>
2026-04-18 13:38:40 +0300

    Unique Directions FAILED

--- Academy/Models/Direction.cs
+++ Academy/Models/Direction.cs
@@ -1,13 +1,25 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+//using System.ComponentModel.DataAnnotations.Schema;
+
 
 namespace Academy.Models
 {
+    [Index(nameof(direction_name), IsUnique = true)]
     public class Direction
     {
         [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        //[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public byte direction_id { get; set; }
+        [Required]
         public string? direction_name { get; set; }
+        //public override bool Equals(object? other)
+        //{
+        //    return this.direction_name.Equals((other as Direction).direction_name);
+        //}
+        //public override int GetHashCode()
+        //{
+        //    return HashCode.Combine(direction_name);
+        //}
     }
 }
